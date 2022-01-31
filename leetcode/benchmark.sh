660fa0e91edf5cffeb56f4c975ce06c76de418de
Shubham Shah <shubhamshahrising@gmail.com>
2022-01-31 18:51:06 +0530

    Remove unnecessary platform

--- leetcode/benchmark.sh
+++
@@ -1,2 +0,0 @@
-g++ benchmark.cpp -std=c++11 -isystem benchmark/include -Lbenchmark/build/src -lbenchmark -lpthread -o solution.benchmark 
-./solution.benchmark
--- leetcode/install.sh
+++
@@ -1,6 +0,0 @@
-git clone https://github.com/google/benchmark.git
-cd benchmark
-cmake -E make_directory "build"
-cmake -E chdir "build" cmake -DBENCHMARK_DOWNLOAD_DEPENDENCIES=on -DCMAKE_BUILD_TYPE=Release ../
-cmake --build "build" --config Release
-
